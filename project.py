42917cb95de38d415b6a104abc3f1aa40945fe4b
ThomasDelepine <thomas.delepine@live.fr>
2024-01-28 16:14:25 +0100

    parameters optimization

--- project.py
+++ project.py
@@ -264,7 +264,7 @@ class ParallelSearch:
 
 
 class OnePOneWithCleanup:
-    def __init__(self, budget, slope, p):
+    def __init__(self, budget, slope, p, c, alpha=0, y=10):
         #Note that we should re-initialize all dynamic variables if we want to run the same algorithm multiple times
         self.budget = budget
 
@@ -275,6 +275,23 @@ class OnePOneWithCleanup:
 
         self.p = p
 
+        self.alpha = alpha # expected number of added 1s during the second part of the algorithm
+
+        self.y = y #number of offsprings in the second part of the algorithm
+
+        self.c = c #crossover parametre
+
+    def parallelOffspring(self, n, x, p, q):
+        for i in range(n):
+            r = np.random.uniform(0, 1)
+            if(x[i] == 1):
+                if(r < p):
+                    x[i] = 0
+            else:
+                if(r < q):
+                    x[i] = 1
+        return x
+
     def __call__(self, func):
         print("start of a run")
         n = func.meta_data.n_variables
@@ -284,16 +301,16 @@ class OnePOneWithCleanup:
         change_strat = False
         chech_frequency = 166 # steps of checking
         N = 0 # Targer number of 1s
-        for e in range(self.budget -1):
-
+        budget = 1
+        while(budget < self.budget):
             # We check the ompovement slope with bigsteps
-            if(e > 500 and (not change_strat) and e%166 == 0):
+            if(budget > 500 and (not change_strat) and budget%166 == 0):
                 if(best_fitness/check < self.slope):
                     change_strat = True
                 check = best_fitness
 
-            child = best.copy()
             if(not change_strat):
+                child = best.copy()
                 for c in range(n):
                     if child[c] == 1:
                         if np.random.rand() < 2.0 / n:
@@ -301,6 +318,13 @@ class OnePOneWithCleanup:
                     else:
                         if np.random.rand() < 1.0 / n:
                             child[c] = 1 - child[c]
+                score = func(child)
+                budget = budget + 1
+                if score > best_fitness:
+                    best = child
+                    best_fitness = score
+                    N = np.count_nonzero(best)
+
             else:
                 nb1 = np.count_nonzero(child)
                 nb0 = n - nb1
@@ -308,22 +332,88 @@ class OnePOneWithCleanup:
                     q = 0.5
                 else:
                     #q = (N - (1 - self.p)*nb1)/nb0
-                    alpha = 1/n
+                    alpha = 0
                     q = (alpha + nb1*self.p)/nb0
+
+                # generation of lambda offsprings, we keep the best
+
+                best_child = best.copy()
+                best_child = self.parallelOffspring(n, best_child, self.p, q)
+                best_child_score = func(best_child)
+                budget = budget + 1
+
+                for _ in range(self.y - 1):
+                    child = best.copy()
+                    child = self.parallelOffspring(n, child, self.p, q)
+                    score = func(child)
+                    budget = budget + 1
+                    if(score > best_child_score):
+                        best_child_score = score
+                        best_child = child.copy()
+
+                # Crossover phase
+                crossover = best.copy()
                 for i in range(n):
                     r = np.random.uniform(0, 1)
-                    if(child[i] == 1):
-                        if(r < self.p):
-                            child[i] = 0
-                    else:
-                        if(r < q):
-                            child[i] = 1
+                    if(r < c):
+                        crossover[i] = best_child[i]
+                crossover_score = func(crossover)
+                budget = budget + 1
 
-            score = func(child)
-            if score > best_fitness:
-                best = child
-                best_fitness = score
-                N = np.count_nonzero(best)
+                # We keep the best generated solution
+                if(crossover_score > best_fitness):
+                    best = crossover.copy()
+                    best_fitness = crossover_score
+                if(best_child_score > best_fitness):
+                    best = best_child.copy()
+                    best_fitness = best_child_score
+                
+
+k = 1
+
+# k=5, 1.0005, k/800, 0.75, alpha=0, y=10 : 11119
+
+# k=2, 1.0005, k/800, 0.75, alpha=0, y=10 : 11242
+
+# k=1, 1.0005, k/800, 0.75, alpha=0, y=10 : 11256
+
+# k=.85, 1.0005, k/800, 0.75, alpha=0, y=10 : 11243
+
+# k=.7, 1.0005, k/800, 0.75, alpha=0, y=10 : 11247
+
+# k=.1, 1.0005, k/800, 0.75, alpha=0, y=10 : 11093
+
+
+# k=1, 1.0005, k/800, 0.25, alpha=0, y=10 : 11248
+
+# k=1, 1.0005, k/800, 0.5, alpha=0, y=10 : 11261
+
+# k=1, 1.0005, k/800, 0.75, alpha=0, y=10 : 11256
+
+# k=1, 1.0005, k/800, 0.9, alpha=0, y=10 : 11251
+
+
+
+# k=1, 1.0005, k/800, 0.5, alpha=0, y=10 : 11261
+
+# k=1, 1.0005, k/800, 0.5, alpha=1, y=10 : 11276
+
+# k=1, 1.0005, k/800, 0.5, alpha=2, y=10 : 11245
+
+
+# k=1, 1.0000, k/800, 0.5, alpha=1, y=10 : 11248
+
+# k=1, 1.0001, k/800, 0.5, alpha=1, y=10 : 11256
+
+# k=1, 1.0002, k/800, 0.5, alpha=1, y=10 : 11278
+
+# k=1, 1.0005, k/800, 0.5, alpha=1, y=10 : 11276
+
+# k=1, 1.0008, k/800, 0.5, alpha=1, y=10 : 11239
+
+
+# k=1, 1.0005, k/800, 0.5, alpha=1, y=10 : 11276
+
 
 
 
@@ -332,10 +422,10 @@ Algs = [RandomSearch,                   #0
         MutationByProgress,             #2
         RandomFromSmallSpeedup,         #3
         WeightedImpact,                 #4
-        WeightedImpactMutation(BUDGET, max_bucket_percent=0.1),         #5
+        WeightedImpactMutation(BUDGET, max_bucket_percent=0.1),            #5
         ParallelSearch(BUDGET),         #6
-        SmallToLargeWithCleanup(BUDGET, max_bucket_percent=0.1),        #7
-        OnePOneWithCleanup(BUDGET, 1.0005, 2/800),     #8
+        SmallToLargeWithCleanup(BUDGET, max_bucket_percent=0.1),           #7
+        OnePOneWithCleanup(BUDGET, 1.0005, k/800, 0.5, alpha=1, y=10),     #8
         ]                 
 
 Alg = Algs[8]          
@@ -348,9 +438,11 @@ exp = Experiment(
     [1],                                            # list of problem dimensions
     problem_class = ProblemClass.GRAPH,             # the problem type, function ids should correspond to problems of this type
     njobs = 1,                                      # the number of parallel jobs for running this experiment
-    reps = 3,                                       # the number of repetitions for each (id x instance x dim)
+    reps = 10,                                       # the number of repetitions for each (id x instance x dim)
     logged_attributes = [                           # list of the tracked variables, must be available on the algorithm instance
     ],
 )
 
-exp.run()
+tmp = exp.run()
+
+print(tmp)
